022448144352f05606c86d826d3dcb2d2d2c2fb5
Tayná Martins Ramos <tayna.ramos@b2wdigital.com>
2021-05-29 19:46:26 -0300

    Adição de icones

--- src/pages/Agendamento/index.js
+++ src/pages/Agendamento/index.js
@@ -1,18 +1,76 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
+import CreateRoundedIcon from '@material-ui/icons/CreateRounded'
+import { Divider, Button, Box } from '@material-ui/core'
 
 import { Container } from './style'
 
+import calendar from 'assets/images/calendar-dark.svg'
+import profile from 'assets/images/photo-profile.png'
+import folder from 'assets/images/folder.svg'
+import user from 'assets/images/user-blue.svg'
+
 export const Agendamento = () => {
+
+  const [isOpen, setIsOpen] = useState(false)
 
   return (
     <Container>
-      
+
       <section className={'menu'}>
 
-        <div className={'menu-logo'}></div>
-        <div className={'menu-profile'}></div>
-        <div className={'menu-tabs'}></div>
-        
+        <div className={'menu-logo'}>
+          <img src={calendar} alt={''}></img>
+          <h3>Agendamento Online</h3>
+        </div>
+
+        <div className={'menu-profile'}>
+
+          <img src={profile} alt={''}></img>
+
+          <div className={'profile-select'}>
+
+            <div className={'profile-select-text'}>
+              <span>Seja bem vindo</span>
+              {/* depois substituir por {nome} */}
+              <span id={'name'}>Johnny Clark</span>
+            </div>
+
+            <ExpandMoreIcon fontSize="small" onClick={() => setIsOpen(!isOpen)} className={isOpen ? 'icon-expanded' : ''}  />
+
+          </div>
+
+        </div>
+
+        <Box className={isOpen ? 'profile-select-expanded' : 'profile-select-closed'}>
+          <div className={'select-option'}>
+            <img src={user} alt={''} />
+            <span>Meu perfil</span>
+          </div>
+
+          <div className={'select-option'}>
+            <img src={user} alt={''} />
+            <span>Sair</span>
+          </div>
+        </Box>
+
+        <Divider variant="middle" className={'divider'} />
+
+        <div className={'menu-tabs'}>
+
+          <Button variant={'contained'} className={'tabs-btn'}>
+            <img src={folder} alt={''} className={'tabs-btn-icon'} id={'folder'} />
+            <span>Meus agendamentos</span>
+          </Button>
+
+          <Button variant={'contained'} className={'tabs-btn'}>
+            <CreateRoundedIcon fontSize="small" className={'tabs-btn-icon'} />
+            <span>Agendamentar</span>
+          </Button>
+
+        </div>
+
       </section>
 
       <section className={'main'}>
--- src/pages/Agendamento/style.js
+++ src/pages/Agendamento/style.js
@@ -1,5 +1,122 @@
 import styled from 'styled-components'
 
+import folderWhite from 'assets/images/folder-white.svg'
+import folder from 'assets/images/folder.svg'
+
 export const Container = styled.main`
+
+.menu {
+    width: 20%;
+    height: 100%;
+    min-height: 100vh;
+    border-right: solid 1px #00B3F3;
+    padding: 1.5rem;
+}
+
+.menu-logo {
+    width: 100%;
+    display: flex;
+    justify-content: flex-start;
+    margin: 1.3rem 0;
+}
+
+.menu-logo img {
+    color: #1E293B;
+    width: 36px;
+    margin-right: 20px;
+}
+
+.menu-logo h3 {
+    width: 50px;
+    margin: 0;
+}
+
+.menu-profile {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    /* margin-bottom: 2.5rem; */
+}
+
+.menu-profile > img {
+    width: 25%;
+}
+
+.profile-select {
+    display: flex;
+    align-items: center;
+}
+
+.profile-select-text{
+    display: flex;
+    flex-direction: column;
+    margin-right: 1rem;
+}
+
+#name {
+    font-family: 'PoppinsBold';
+    color: #00B3F3;
+}
+
+.icon-expanded {
+    transform: rotate(180deg);
+}
+
+.profile-select-expanded {
+    display: flex;
+    flex-direction: column;
+    padding: 0.5rem;
+    border: solid 1px #00B3F3;
+    border-radius: 4px;
+    margin:1rem auto;
+
+}
+
+.profile-select-closed {
+    display: none;
+}
+
+.select-option img {
+    margin-right: 0.8rem;
+}
+
+.divider {
+    margin: 2.5rem 0 1rem 0;
+}
+
+.menu-tabs {
+    width: 100%;
+}
+
+.tabs-btn{
+    color:#1E293B;
+    margin: 0.5rem 0;
+    display: flex;
+    justify-content: flex-start;
+    width: 100%;
+    font-family: 'PoppinsRegular';
+    font-size: 16px;
+    text-transform: capitalize;
+    background-color: #FFFFFF;
+    box-shadow: none;
+}
+
+.tabs-btn:hover {
+    background-color: #00B3F3;
+    color: #fff;
+}
+
+.tabs-btn #folder {
+    max-height: 25px;
+}
+
+.tabs-btn-icon {
+    margin-right: 0.5rem;
+    fill: #00B3F3;
+}
+
+.tabs-btn:hover .tabs-btn-icon {
+    fill: #fff;
+}
 
 `
