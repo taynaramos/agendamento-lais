6341966c5e3c6bf3e8e78d6922273dab152d7962
Tayná Martins Ramos <tayna.ramos@b2wdigital.com>
2021-06-02 18:22:06 -0300

    Validação

--- src/components/FormLogin/index.js
+++ src/components/FormLogin/index.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { Link, useHistory } from 'react-router-dom'
 
 import { Formik } from 'formik';
 
 import { Paper, Button, TextField } from '@material-ui/core';
+
+import api from 'services/api'
 
 import { Container } from './style'
 
@@ -15,90 +17,102 @@ export const FormLogin = () => {
         password: '',
     }
 
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
     let history = useHistory();
 
     const userDefault = {
         name: 'Johnny Clark'
     }
 
+    const handlePost = async () => {
+        const body = {
+            email: email,
+            password: password
+        }
+
+        const result = await api.post('/auth/login', body)
+        history.push(`/agendamentos/${userDefault.name}`)
+    }
+
     return (
         <Container className={'main-form'}>
 
-                <Paper className={'form'}>
+            <Paper className={'form'}>
 
-                    <p>Preencha os campos abaixo</p>
+                <p>Preencha os campos abaixo</p>
 
-                    <Formik
-                        initialValues={initialValues}
-                        validate={values => {
-                            const errors = {};
-                            if (!(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)))
-                                errors.email = "Email não válido"
-                            if (!(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/.test(values.password)))
-                                errors.password = "Mínimo de oito caracteres, pelo menos uma letra maiúscula, uma letra minúscula, um número e um caractere especial"
+                <Formik
+                    initialValues={initialValues}
+                    validate={values => {
+                        const errors = {};
+                        if (!(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)))
+                            errors.email = "Email não válido"
 
-                            return errors;
-                        }}
-                        onSubmit={(values, { setSubmitting }) => {
-                            setTimeout(() => {
+                        return errors;
+                    }}
+                    onSubmit={(values, { setSubmitting }) => {
+                        setTimeout(() => {
 
-                                // alert(JSON.stringify(values, null, 2));
+                            // alert(JSON.stringify(values, null, 2));
 
-                                alert("Dados salvos com sucesso!");
-                                history.push(`/agendamentos/${userDefault.name}`)
+                            setPassword(values.password)
+                            setEmail(values.email)
 
-                                setSubmitting(false);
-                            }, 400);
-                        }}
-                    >
-                        {({
-                            values,
-                            errors,
-                            touched,
-                            handleChange,
-                            handleSubmit,
-                        }) => (
-                                <form onSubmit={handleSubmit}>
+                            handlePost()
+                            setSubmitting(false);
+                        }, 400);
+                    }}
+                >
+                    {({
+                        values,
+                        errors,
+                        touched,
+                        handleChange,
+                        handleSubmit,
+                    }) => (
+                            <form onSubmit={handleSubmit}>
 
-                                    <div className={'form-field'}>
-                                        <span>Email</span>
-                                        <TextField
-                                            fullWidth
-                                            variant="outlined"
-                                            id="email"
-                                            name="email"
-                                            value={values.email}
-                                            onChange={handleChange}
-                                            error={touched.email && Boolean(errors.email)}
-                                            helperText={touched.email && errors.email}
-                                        />
-                                    </div>
+                                <div className={'form-field'}>
+                                    <span>Email</span>
+                                    <TextField
+                                        fullWidth
+                                        variant="outlined"
+                                        id="email"
+                                        name="email"
+                                        value={values.email}
+                                        onChange={handleChange}
+                                        error={touched.email && Boolean(errors.email)}
+                                        helperText={touched.email && errors.email}
+                                    />
+                                </div>
 
-                                    <div className={'form-field'}>
-                                        <span>Senha</span>
-                                        <TextField
-                                            fullWidth
-                                            variant="outlined"
-                                            id="password"
-                                            name="password"
-                                            type="password"
-                                            value={values.password}
-                                            onChange={handleChange}
-                                            error={touched.password && Boolean(errors.password)}
-                                            helperText={touched.password && errors.password}
-                                        />
-                                    </div>
+                                <div className={'form-field'}>
+                                    <span>Senha</span>
+                                    <TextField
+                                        fullWidth
+                                        variant="outlined"
+                                        id="password"
+                                        name="password"
+                                        type="password"
+                                        value={values.password}
+                                        onChange={handleChange}
+                                        error={touched.password && Boolean(errors.password)}
+                                        helperText={touched.password && errors.password}
+                                    />
+                                </div>
 
-                                    <Link to={'/mudar-senha'}><p className={'forgot-password'}>Esqueceu sua senha?</p></Link>
+                                <Link to={'/mudar-senha'}><p className={'forgot-password'}>Esqueceu sua senha?</p></Link>
 
-                                    <Button variant="contained" fullWidth type="submit" className={'form-btn'}>
-                                        Entrar
+                                <Button variant="contained" fullWidth type="submit" className={'form-btn'}>
+                                    Entrar
                                     </Button>
-                                </form>
-                            )}
-                    </Formik>
+                            </form>
+                        )}
+                </Formik>
 
-                </Paper>
+            </Paper>
 
         </Container>
     )
